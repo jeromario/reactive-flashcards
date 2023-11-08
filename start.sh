3d5391526bb2df78238f9e541cc88b661538bd30
origin <https://github.com/jeromario/desafio_poo.git>
2023-11-08 08:48:12 -0300

    configuration

---
+++ start.sh
@@ -0,0 +1,4 @@
+gradle clear
+gradle botjar
+#java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -jar build/lib/reactive-flashcards-1.0.0.jar
+java -jar build/lib/reactive-flashcards-1.0.0.jar
